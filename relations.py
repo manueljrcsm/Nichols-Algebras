01e6d4caf18e76740d0d1edc49a9ef1def888229
Manuel Martins <manueljrcsm@gmail.com>
2020-03-31 19:31:14 +0200

    Testing. Added the code that I had done before to the algebra file.

--- relations.py
+++ relations.py
@@ -16,4 +16,22 @@ def degree(word):
                 break     
     return deg #in Z^2
 
+# consider importing sage.combinat.q_analogues.q_int outside of Anaconda (running via a SAGE Jupyter Notebook), similar for q_factorial
+def q_int(n,q):
+    # only works with 
+    result = 0
+    for i in range(n):
+        result+=q**i
+    return result
+
+def q_factorial(n,q):
+    result = 1
+    for i in range(n):
+        result *= q_int(i+1,q)
+    return result
+
+def q_bilinear(first,second):
+    
+    return
+        
     
